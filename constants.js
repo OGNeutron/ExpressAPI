75b8d0dc3d907e45b3e3f4dd07c6f32b37340cab
OGNeutron <scottberry91@gmail.com>
2017-02-08 11:39:46 +0000

    Changes ENV port

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports.setupGlobals = () => {
     process.env.MONGO_CONNECTION = 'mongodb://expressapi:expressapi@ds145649.mlab.com:45649/expressapi';
-    process.env.PORT = 4000;
+    process.env.PORT = 3000;
     process.env.SECRET = "NeutronStar"
 }
